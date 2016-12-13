07f63febf7536edddb41fb183ad0d3de027076ff
ZhluktenkoDmytro <d1mnewz@gmail.com>
2016-12-13 13:49:30 +0200

    Add project files.

---
+++ FollowersTrack/Tracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FollowersTrack
+{
+   public static class Tracker // via vkNet api
+    {
+        public static void WriteLog(Exception ex)
+        {
+            StreamWriter sw = null;
+        }
+
+    }
+}
